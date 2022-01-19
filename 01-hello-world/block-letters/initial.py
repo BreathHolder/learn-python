bc9680400240d8812192f0bd7009b52567b12c61
PaperTurkey <mathew.sturdevant@gmail.com>
2022-01-18 22:22:19 -0500

    first commit

---
+++ 01-hello-world/block-letters/initial.py
@@ -0,0 +1,10 @@
+# Mat Sturdevant
+# I am hoping to actually pick up python this time (try 10)
+
+print("M   M  SSS ")
+print("MM MM S   S")
+print("MM MM S    ")
+print("M M M  SSS ")
+print("M   M     S")
+print("M   M S   S")
+print("M   M  SSS")
